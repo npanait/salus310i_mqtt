90dab66577fee08010ca10f54877cd870f265f42
Nicolae Panait <npanait@gmail.com>
2022-01-23 16:19:20 +0200

    add mqtt publish subscribe

--- python-src/main.py
+++ python-src/main.py
@@ -11,10 +11,58 @@ import logging
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 import shutil
+import json 
+import paho.mqtt.client as mqtt
+
 
 __author__ = "Nicolae Panait"
 __version__ = "0.2.0"
 __license__ = "MIT"
+
+# MQTT constants
+MQTT_HOST="192.168.64.128"
+MQTT_PORT=1883
+MQTT_KEEPALIVE_INTERVAL = 45
+MQTT_TOPIC = "GenusOne"
+MQTT_MESSAGE = {"Sensor Time":datetime.now().isoformat(),
+                        "CurrentTemperature":{"Value":0, "Type":"temp", "Unit":"C"},
+                        "TargetTemperature":{"Value":0,  "Type":"temp", "Unit":"C"}, 
+                        "Program":{"Value":"HEATING AUTO"},
+                        "Gas":{"Value":0,  "Type":"boolean"}
+                }
+
+
+#MQTT_MSG=json.dumps({"sepalLength": "6.4","sepalWidth":  "3.2","petalLength": "4.5","petalWidth":  "1.5"});
+# Define on_publish event function
+def on_publish(client, userdata, mid):
+    pass
+    #print ("Message Published...")
+
+def on_connect(client, userdata, flags, rc):
+    client.subscribe(MQTT_TOPIC)
+    #client.publish(MQTT_TOPIC, MQTT_MSG)
+
+def on_message(client, userdata, msg):
+    print(msg.topic)
+    print(msg.payload) # <- do you mean this payload = {...} ?
+    payload = json.loads(msg.payload) # you can use json.loads to convert string to json
+    #print(payload['sepalWidth']) # then you can check the value
+    client.disconnect() # Got message then disconnect
+
+# Initiate MQTT Client
+mqttc = mqtt.Client()
+
+# Register publish callback function
+mqttc.on_publish = on_publish
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+
+# Connect with MQTT Broker
+mqttc.username_pw_set(secrets.mqtt_user, secrets.mqtt_pass)
+mqttc.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL)
+
+# Loop forever
+#mqttc.loop_forever()
 
 #This URL will be the URL that your login form points to with the "action" tag.
 POST_LOGIN_URL = 'https://salus-it500.com/public/login.php?lang=en'
@@ -26,6 +74,11 @@ payload = {
     'password': secrets.salus_pass,
     'login': "Login"
 }
+
+def nested_set(dic, keys, value):
+    for key in keys[:-1]:
+        dic = dic.setdefault(key, {})
+    dic[keys[-1]] = value
 
 def main():
     """ Main entry point of the app """
@@ -46,20 +99,31 @@ def main():
                 
                 #request data parsed by beautiful soup
                 soup = bs.BeautifulSoup(r.text,'html.parser')
-                # Finds the Current temperature
-                logging.info('Current temperature is: {:03.1f} with target {:03.1f}'.format(float(soup.find(id='current_room_tempZ1').get_text()),float(soup.find(id='current_tempZ1').get_text()) ))  #<p id="current_room_tempZ1">23<span class="lastDigit">.9</span></p>
-                # Finds the Heating note indicator
-                logging.info(soup.find('p', class_='heatingNote').get_text().strip())  #<p class="heatingNote heatingAuto">HEATING AUTO</p>
+                # Finds the Current temperature and saves it to the dictionaty
+                # nested_set(d, ['person', 'address', 'city'], 'New York')
+                nested_set(MQTT_MESSAGE, ["CurrentTemperature","Value"], float( soup.find(id='current_room_tempZ1').get_text() ) )
+                nested_set(MQTT_MESSAGE, ["TargetTemperature","Value"], float( soup.find(id='current_tempZ1').get_text() ) )
+                logging.info('Current temperature is: {:03.1f} with target {:03.1f}'.format(MQTT_MESSAGE["CurrentTemperature"]["Value"],MQTT_MESSAGE["TargetTemperature"]["Value"]) )  #<p id="current_room_tempZ1">23<span class="lastDigit">.9</span></p>
+                # Finds the Heating note indicator and saves it to dictionary
+                nested_set(MQTT_MESSAGE, ["Program","Value"],soup.find('p', class_='heatingNote').get_text().strip() )
+                logging.info(MQTT_MESSAGE["Program"]["Value"])  #<p class="heatingNote heatingAuto">HEATING AUTO</p>
                 # <img id="CH1onOff" class="CH1onOff" src="assets/flame.png" alt="Zone 1 is on">
                 
+
                 flame = soup.find("img", class_="CH1onOff display",id="CH1onOff") #soup.find('img', id_ ='CH1onOff')  class="CH1onOff display"
                 #print(flame)
-
-                logging.info('Gaz-Pornit' if flame else 'Gaz-Oprit')
+                if (flame is None):
+                    nested_set(MQTT_MESSAGE, ["Gas","Value"],0)
+                    logging.info('Gaz-Pornit' if flame else 'Gaz-Oprit')
+                else:
+                    nested_set(MQTT_MESSAGE, ["Gas","Value"],1)
+                    logging.info('Gaz-Pornit' if flame else 'Gaz-Oprit')
+                
+                mqttc.publish(MQTT_TOPIC, payload=json.dumps(MQTT_MESSAGE), qos=0, retain=False)
             else:
                 logging.error('Salus server not available')
             time.sleep(60)
-        
+
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
